e7d0228354f58ed4c80d3916e3de7e976d25f2cb
Siberian-Breaks <sprowlr@wit.edu>
2022-07-29 14:37:39 -0400

    Uploaded ue5 project files

---
+++ ue5-project-files/Plugins/VictoryPlugin5/Source/VictoryBPLibrary/VictoryBPLibrary.Build.cs
@@ -0,0 +1,58 @@
+// Some copyright should be here...
+
+using UnrealBuildTool;
+
+public class VictoryBPLibrary : ModuleRules
+{
+	public VictoryBPLibrary(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PublicIncludePaths.AddRange(
+			new string[] {
+				// ... add public include paths required here ...
+			}
+			);
+				
+		
+		PrivateIncludePaths.AddRange(
+			new string[] {
+				// ... add other private include paths required here ...
+			}
+			);
+			
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				// ... add other public dependencies that you statically link with here ...
+			}
+			);
+			
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"InputCore",
+				"RHI",
+				"Slate",
+				"SlateCore",
+				"ApplicationCore",
+				"AppFramework",			//For Color Picker! ♥ Rama
+				"UMG", "Slate", "SlateCore",
+			
+			}
+			);
+		
+		
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+				// ... add any modules that your module loads dynamically here ...
+			}
+			);
+	}
+}
